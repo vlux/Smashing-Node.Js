3a8c569515548f8ad374b7743a6a74f621aca463
Jmcus <vluxyue@gmail.com>
2014-08-29 21:39:07 +0800

    get/post

---
+++ http_read.js
@@ -0,0 +1,25 @@
+//P87
+
+require('http').createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'image/png'});
+    var stream = require('fs').createReadStream('image.png');
+    stream.on('data',function(data){
+        res.write(data);
+    });
+    stream.on('end',function(data){
+        res.end();
+    });
+}).listen(3000);
+
+//好处：高效的内存分配，要是对每个请求在写入前都完全把图片信息读取完(fs.readFile)在处理大量请求时会消耗大量内存
+//     数据一旦就绪就可以立刻写入了
+
+
+
+
+//简洁写法:
+
+require('http').createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'image/png'});
+    require('fs').createReadStream('image.png').pipe(res);
+}).listen(3000);
---
+++ http_test.js
@@ -0,0 +1,9 @@
+//P85
+
+require('http').createServer(function(req,res){
+    res.writeHead(200);
+    res.write('Hello');
+    setTimeout(function(){
+        res.end('World');
+    },500)
+}).listen(3000);
