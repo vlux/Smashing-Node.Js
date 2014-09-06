a7828408a16c944e368cb251d0c88167da1b29ce
Jmcus <vluxyue@gmail.com>
2014-09-06 16:08:28 +0800

    html

---
+++ Chat/chat.js
@@ -0,0 +1,86 @@
+//P187
+
+window.onload = function(){
+    var socket = io.connect();
+    socket.on('connect',function(){
+        //通过join事件发送昵称
+        socket.emit('join',prompt('What is ur nickname?'));
+
+        //显示聊天窗口
+        document.getElementById('chat').style.display = 'block';
+
+        socket.on('announcement',function(msg){
+            var li = document.createElement('li');
+            li.className = 'announcement';
+            li.innerHTML = msg;
+            document.getElementById('message').appendChild(li);
+        });
+    });
+
+    function addMessage(from,text){
+        var li = document.createElement('li');
+        li.className = 'message';
+        li.innerHTML = '<b>' + from + '</b>' + text;
+        document.getElementById('message').appendChild(li);
+    }
+
+    var input = document.getElementById('input');
+    document.getElementById('form').onsubmit = function(){
+        addMessage('me',input.value);
+        socket.emit('text',input.value);
+
+        //重置输入框
+        input.value = '';
+        input.focus();
+
+        return false;
+    }
+
+    socket.on('text',addMessage);
+
+    var playing = document.getElementById('playing');
+    function play(song){
+        if(!song)
+            return;
+        playing.innerHTML = '<br><b>Now Playing: </b>' + song.ArtistName +' ' + song.SongName + '<br>';
+
+        var iframe = document.createElement('iframe');
+        iframe.frameborder = 0;
+        iframe.src = song.Url;
+        playing.appendChild(iframe);
+    }
+
+    socket.on('song',play);
+
+    //查询表单
+    var form = document.getElementById('dj');
+    var results = document.getElementById('results');
+    form.style.display = 'block';
+    form.onsubmit = function(){
+        results.innerHTML = '';
+        socket.emit('search',document.getElementById('s').value,function(songs){
+            for(var i = 9, i = songs.length ; i < l; i++){
+                (function(song){
+                    var result = document.createElement('li');
+                    result.innerHTML = song.ArtistName + ' - <b>' + song.SongName + '</b>';
+                    var a = document.createElement('a');
+                    a.href = '#';
+                    a.innerHTML = 'Select';
+                    a.onclick = function(){
+                        socket.emit('song',song);
+                        play(song);
+                        return false;
+                    }
+                    result.appendChild(a);
+                    results.appendChild(result);
+                })(songs[i]);                            //学会！！！！
+            }
+        });
+        return false;
+    };
+
+    socket.on('elected',function(){
+        form.className = 'isDJ';
+    });
+    
+}
